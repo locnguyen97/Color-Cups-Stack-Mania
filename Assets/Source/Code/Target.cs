0052cc32a67fdcc6c9e97ff526bf9da982b692b0
locnguyen97 <46517710+locnguyen97@users.noreply.github.com>
2023-11-29 00:21:27 +0700

    c

---
+++ Assets/Source/Code/Target.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public int id = -1;
+    public bool hasSlot = false;
+}
